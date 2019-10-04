7a3790f05ea9a5e8c86932005a5fa9c94cb126a5
olegsher <github@sher.biz>
2019-10-05 01:01:21 +0300

    the same + reformat code

--- src/telran/company/dto/Employee.java
+++ src/telran/company/dto/Employee.java
@@ -1,121 +1,143 @@
 package telran.company.dto;
+
 import java.time.LocalDate;
 
 
-
 public class Employee {
- long id;
- int salary;
- String department;
- String title;
- String name;
+    long id;
+    int salary;
+    String department;
+    String title;
+    String name;
 
- String gender;
- public String getGender() { return gender; }
- public void setGender(String gender) { this.gender = gender; }
+    String gender;
+    // LocalDate from class java.time.Localdate
+    LocalDate employeeBirthDay;
+    LocalDate employeeStartJob;
 
-	// LocalDate from class java.time.Localdate
- LocalDate employeeBirthDay;
- LocalDate employeeStartJob;
+    public Employee(long id, int salary,
+                    String department, String title,
+                    String name) {
 
-
-	//Setters for employee Birthday and startJob
-	public void setEmployeeBirthDay(java.time.LocalDate employeeBirthDay) { this.employeeBirthDay = employeeBirthDay; }
-	public void setEmployeeStartJob(java.time.LocalDate employeeStartJob) { this.employeeStartJob = employeeStartJob; }
-	//Getters for employee Birthday and startJob
-	public java.time.LocalDate getEmployeeBirthDay() { return employeeBirthDay; }
-	public java.time.LocalDate getEmployeeStartJob() { return employeeStartJob; }
-
-
- public int getSalary() {
-	return salary;
-}
- public void setSalary(int salary) {
-	this.salary = salary;
-}
-@Override
-public int hashCode() {
-	final int prime = 31;
-	int result = 1;
-	result = prime * result + ((department == null) ? 0 : department.hashCode());
-	result = prime * result + (int) (id ^ (id >>> 32));
-	result = prime * result + ((name == null) ? 0 : name.hashCode());
-	result = prime * result + salary;
-	result = prime * result + ((title == null) ? 0 : title.hashCode());
-	result = prime * result + ((gender == null) ? 0 : gender.hashCode());
-	return result;
-}
-@Override
-public boolean equals(Object obj) {
-	if (this == obj)
-		return true;
-	if (obj == null)
-		return false;
-	if (getClass() != obj.getClass())
-		return false;
-	telran.company.dto.Employee other = (telran.company.dto.Employee) obj;
-	if (department == null) {
-		if (other.department != null)
-			return false;
-	} else if (!department.equals(other.department))
-		return false;
-	if (id != other.id)
-		return false;
-	if (name == null) {
-		if (other.name != null)
-			return false;
-	} else if (!name.equals(other.name))
-		return false;
-	if (salary != other.salary)
-		return false;
-	if (title == null) {
-		if (other.title != null)
-			return false;
-	} else if (!title.equals(other.title))
-		return false;
-	return true;
-}
-public String getDepartment() {
-	return department;
-}
-public void setDepartment(String department) {
-	this.department = department;
-}
-public String getTitle() {
-	return title;
-}
-public void setTitle(String title) {
-	this.title = title;
-}
-public String getName() {
-	return name;
-}
-public void setName(String name) {
-	this.name = name;
-}
-public long getId() {
-	return id;
-}
-public Employee(long id, int salary,
-		 String department, String title,
-		 String name) {
-	
-	this.id = id;
-	this.salary = salary;
-	this.department = department;
-	this.title = title;
-	this.name = name;
-	this.employeeBirthDay = LocalDate.MIN; // if value is not set, the System.out.println(LocalDate.MIN) == -9999999-01-01 - wrong number
-	this.employeeStartJob = LocalDate.MIN; // if value is not set, the System.out.println(LocalDate.MIN) == -9999999-01-01 - wrong number
-	this.gender = "Not set";
+        this.id = id;
+        this.salary = salary;
+        this.department = department;
+        this.title = title;
+        this.name = name;
+        this.employeeBirthDay = LocalDate.MIN; // if value is not set, the System.out.println(LocalDate.MIN) == -9999999-01-01 - wrong number
+        this.employeeStartJob = LocalDate.MIN; // if value is not set, the System.out.println(LocalDate.MIN) == -9999999-01-01 - wrong number
+        this.gender = "Not set";
 
 
-}
+    }
 
-@Override
-public String toString() {
-	return "Employee [id=" + id + ", salary=" + salary + ", department=" + department + ", title=" + title + ", name="
-			+ name + "]";
-}
- 
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    //Getters for employee Birthday and startJob
+    public java.time.LocalDate getEmployeeBirthDay() {
+        return employeeBirthDay;
+    }
+
+    //Setters for employee Birthday and startJob
+    public void setEmployeeBirthDay(java.time.LocalDate employeeBirthDay) {
+        this.employeeBirthDay = employeeBirthDay;
+    }
+
+    public java.time.LocalDate getEmployeeStartJob() {
+        return employeeStartJob;
+    }
+
+    public void setEmployeeStartJob(java.time.LocalDate employeeStartJob) {
+        this.employeeStartJob = employeeStartJob;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((department == null) ? 0 : department.hashCode());
+        result = prime * result + (int) (id ^ (id >>> 32));
+        result = prime * result + ((name == null) ? 0 : name.hashCode());
+        result = prime * result + salary;
+        result = prime * result + ((title == null) ? 0 : title.hashCode());
+        result = prime * result + ((gender == null) ? 0 : gender.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        telran.company.dto.Employee other = (telran.company.dto.Employee) obj;
+        if (department == null) {
+            if (other.department != null)
+                return false;
+        } else if (!department.equals(other.department))
+            return false;
+        if (id != other.id)
+            return false;
+        if (name == null) {
+            if (other.name != null)
+                return false;
+        } else if (!name.equals(other.name))
+            return false;
+        if (salary != other.salary)
+            return false;
+        if (title == null) {
+            return other.title == null;
+        } else return title.equals(other.title);
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee [id=" + id + ", salary=" + salary + ", department=" + department + ", title=" + title + ", name="
+                + name + "]";
+    }
+
 }
--- src/telran/company/service/Company.java
+++ src/telran/company/service/Company.java
@@ -1,83 +1,86 @@
 package telran.company.service;
 
-import java.util.Arrays;
-import java.util.function.Predicate;
-
-import telran.company.dto.Employee;
-
 public class Company {
-telran.company.dto.Employee[]employees=new telran.company.dto.Employee[0];
-public boolean hire(telran.company.dto.Employee empl) {
-	if(getEmployee(empl.getId())!=null) {
-		return false;
-	}
-	employees= java.util.Arrays.copyOf(employees,employees.length+1);
-	employees[employees.length-1]=empl;
-	return true;
-}
-public telran.company.dto.Employee getEmployee(long id) {
-	for(int i=0; i<employees.length; i++) {
-		if(employees[i].getId()==id) {
-			return employees[i];
-		}
-	}
-	return null;
-}
-public boolean fire(long id) {
-	for(int i=0; i<employees.length; i++) {
-		if(employees[i].getId()==id) {
-			telran.company.dto.Employee tmpEmployees[]=
-					new telran.company.dto.Employee[employees.length-1];
-			System.arraycopy
-		(employees, 0, tmpEmployees, 0, i);
-			System.arraycopy
-			(employees, i+1, tmpEmployees, i,
-					tmpEmployees.length-i);
-			employees = tmpEmployees;
-			return true;
-		}
-	}
-	return false;
-}
-public telran.company.dto.Employee[] getEmployeesBySalary(int salaryFrom, int salaryTo) {
-	telran.company.dto.Employee[]tmpEmployees = new telran.company.dto.Employee[employees.length];
-	int indTmp=0;
-	for(int i=0; i<employees.length; i++) {
-		int salary = employees[i].getSalary();
-		if(salary >= salaryFrom && salary <= salaryTo) {
-			tmpEmployees[indTmp++]=employees[i];
-		}
-	}
-	return java.util.Arrays.copyOf(tmpEmployees, indTmp);
-}
-public telran.company.dto.Employee[] getEmployeesByDepartment(String department) {
-	telran.company.dto.Employee[]tmpEmployees = new telran.company.dto.Employee[employees.length];
-	int indTmp=0;
-	for(int i=0; i<employees.length; i++) {
-		if(employees[i].getDepartment().equalsIgnoreCase(department)){
-			tmpEmployees[indTmp++]=employees[i];
-		}
-	}
-	return java.util.Arrays.copyOf(tmpEmployees, indTmp);
-}
-public telran.company.dto.Employee[] getEmployeesByTitle(String title) {
-	return getEmployeesByPredicate
-			(new PredicateTitle(title));
-}
-private telran.company.dto.Employee[] getEmployeesByPredicate
-(java.util.function.Predicate<telran.company.dto.Employee> predicate) {
-	telran.company.dto.Employee []tmpEmployees=new telran.company.dto.Employee[employees.length];
-	int indTmp=0;
-	for(int i=0; i<employees.length; i++) {
-		if(predicate.test(employees[i])) {
-			tmpEmployees[indTmp++]=employees[i];
-		}
-	}
-	return java.util.Arrays.copyOf(tmpEmployees, indTmp);
-}
-public telran.company.dto.Employee[] getEmployeesSalarySortedDesc() {
-	telran.company.dto.Employee[] res= java.util.Arrays.copyOf(employees, employees.length);
-	java.util.Arrays.sort(res,new SalaryDescComparator());
-	return res;
-}
+    telran.company.dto.Employee[] employees = new telran.company.dto.Employee[0];
+
+    public boolean hire(telran.company.dto.Employee empl) {
+        if (getEmployee(empl.getId()) != null) {
+            return false;
+        }
+        employees = java.util.Arrays.copyOf(employees, employees.length + 1);
+        employees[employees.length - 1] = empl;
+        return true;
+    }
+
+    public telran.company.dto.Employee getEmployee(long id) {
+        for (int i = 0; i < employees.length; i++) {
+            if (employees[i].getId() == id) {
+                return employees[i];
+            }
+        }
+        return null;
+    }
+
+    public boolean fire(long id) {
+        for (int i = 0; i < employees.length; i++) {
+            if (employees[i].getId() == id) {
+                telran.company.dto.Employee[] tmpEmployees =
+                        new telran.company.dto.Employee[employees.length - 1];
+                System.arraycopy
+                        (employees, 0, tmpEmployees, 0, i);
+                System.arraycopy
+                        (employees, i + 1, tmpEmployees, i,
+                                tmpEmployees.length - i);
+                employees = tmpEmployees;
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public telran.company.dto.Employee[] getEmployeesBySalary(int salaryFrom, int salaryTo) {
+        telran.company.dto.Employee[] tmpEmployees = new telran.company.dto.Employee[employees.length];
+        int indTmp = 0;
+        for (int i = 0; i < employees.length; i++) {
+            int salary = employees[i].getSalary();
+            if (salary >= salaryFrom && salary <= salaryTo) {
+                tmpEmployees[indTmp++] = employees[i];
+            }
+        }
+        return java.util.Arrays.copyOf(tmpEmployees, indTmp);
+    }
+
+    public telran.company.dto.Employee[] getEmployeesByDepartment(String department) {
+        telran.company.dto.Employee[] tmpEmployees = new telran.company.dto.Employee[employees.length];
+        int indTmp = 0;
+        for (int i = 0; i < employees.length; i++) {
+            if (employees[i].getDepartment().equalsIgnoreCase(department)) {
+                tmpEmployees[indTmp++] = employees[i];
+            }
+        }
+        return java.util.Arrays.copyOf(tmpEmployees, indTmp);
+    }
+
+    public telran.company.dto.Employee[] getEmployeesByTitle(String title) {
+        return getEmployeesByPredicate
+                (new PredicateTitle(title));
+    }
+
+    private telran.company.dto.Employee[] getEmployeesByPredicate
+            (java.util.function.Predicate<telran.company.dto.Employee> predicate) {
+        telran.company.dto.Employee[] tmpEmployees = new telran.company.dto.Employee[employees.length];
+        int indTmp = 0;
+        for (int i = 0; i < employees.length; i++) {
+            if (predicate.test(employees[i])) {
+                tmpEmployees[indTmp++] = employees[i];
+            }
+        }
+        return java.util.Arrays.copyOf(tmpEmployees, indTmp);
+    }
+
+    public telran.company.dto.Employee[] getEmployeesSalarySortedDesc() {
+        telran.company.dto.Employee[] res = java.util.Arrays.copyOf(employees, employees.length);
+        java.util.Arrays.sort(res, new SalaryDescComparator());
+        return res;
+    }
 }
